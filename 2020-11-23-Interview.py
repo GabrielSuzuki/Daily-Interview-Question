326e687d4a0cd6424e5e4d5f31625b9c5444a87a
GabrielSuzuki <60160196+GabrielSuzuki@users.noreply.github.com>
2020-11-23 14:00:11 -0800

    finished 11/23/2020

--- 2020-11-23-Interview.py
+++ 2020-11-23-Interview.py
@@ -1,24 +1,26 @@
 #Given a list of numbers with only 3 unique numbers (1, 2, 3), sort the list in O(n) time.
 #Input: [3, 3, 2, 1, 3, 2, 1]
 #Output: [1, 1, 2, 2, 3, 3, 3]
-i = 0
-j = 0
+
 def sortNums(nums):
+  i = 0
+  j = 0
   # Fill this in.
   sortedlist = []
   for k in nums:
     if nums[k] == 1:
       #place at beginning 
-      sortedlist.insert(0,k)
+      sortedlist.insert(0,nums[k])
       i += 1
     elif nums[k] == 2:
       #place at i position
-      sortedlist.insert(i,k)
+      sortedlist.insert(i,nums[k])
+      j += 1
     elif nums[k] == 3:
       #place at end
-      sortedlist.append(k)
+      sortedlist.insert((j+i),nums[k])
   return sortedlist
-print sortNums([3, 3, 2, 1, 3, 2, 1])
+print(sortNums([3, 3, 2, 1, 3, 2, 1]))
 # [1, 1, 2, 2, 3, 3, 3]
 
 #Challenge: Try sorting the list using constant space.
