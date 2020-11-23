3535d99b02a18c5c333572b00b6b0c9f1dfe86a8
GabrielSuzuki <60160196+GabrielSuzuki@users.noreply.github.com>
2020-11-23 12:24:17 -0800

    Create 2020-11-23-Interview.py

---
+++ 2020-11-23-Interview.py
@@ -0,0 +1,9 @@
+#Input: [3, 3, 2, 1, 3, 2, 1]
+#Output: [1, 1, 2, 2, 3, 3, 3]
+def sortNums(nums):
+  # Fill this in.
+
+print sortNums([3, 3, 2, 1, 3, 2, 1])
+# [1, 1, 2, 2, 3, 3, 3]
+
+#Challenge: Try sorting the list using constant space.
