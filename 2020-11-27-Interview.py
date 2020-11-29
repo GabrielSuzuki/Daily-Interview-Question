c86b8c2d6acb18fbe0dfa08c4b652814704b2649
GabrielSuzuki <60160196+GabrielSuzuki@users.noreply.github.com>
2020-11-28 20:01:22 -0800

    unfnished

--- 2020-11-27-Interview.py
+++ 2020-11-27-Interview.py
@@ -11,6 +11,7 @@ class Node:
     self.value = value
 
 def findCeilingFloor(root_node, k, floor=None, ceil=None):
+  tempvarA = []
   # Fill this in.
   if root_node == None:
     #if there are no items in the tree return None
@@ -18,33 +19,70 @@ def findCeilingFloor(root_node, k, floor=None, ceil=None):
   if root_node.value == k:
     #if the root = k return the root as the ceiling/floor
     return (root_node.value, root_node.value)
+  if root_node.value > k:
+    if(root_node.left == None):
+      tempvarA = (floor,root_node.value)
+    else:
+      tempvarA = findCeilingFloor(root_node.left, k, root_node.left.value, root_node.value)
+      #floor = node value
+      #ceiling = tempvarA[1]
+      return (tempvarA[1], root_node.value)
+
   if root_node.value < k:
-    if root_node.right.value > k: 
+    if(root_node.right == None):
+      tempvarA = (root_node.value, ceil)
+    else:
+      tempvarA = findCeilingFloor(root_node.right, k, root_node.right.value, root_node.value)
+        #floor = node value
+        #ceiling = tempvarA[1]
+      return (root_node.value,tempvarA[1]) 
+
+
+
+    #if root_node.right.value > k: 
       #ceiling = root
       #floor = right
-      return (root_node.right.value, root_node.value)
-    else:
-      if(root_node.right == None):
-        #ceiling = root_node
-        #floor = None
-        return (None, root_node.value)
-      else:
-        #Go to the right branch
-        findCeilingFloor(root_node.right, k)
-  elif root_node.value > k:
-    if root_node.left.value < k:
+      #return (root_node.right.value, root_node.value)
+   # if(root_node.right == None):
+      #ceiling = root_node
+      #floor = None
+    #  return (None, root_node.value)
+    #else:
+      #Go to the right branch
+     # findCeilingFloor(root_node.right, k)
+  #elif root_node.value > k:
+    #if root_node.left.value < k:
       #ceiling = root
       #floor = left
-      return (root_node.left.value, root_node.value)
-    else:
-      if(root_node.left == None):
-        #ceiling = root_node
-        #floor = None
-        return (None, root_node.value)
-      else:
-        #Go to the left branch
-        findCeilingFloor(root_node.left, k)
+      #return (root_node.left.value, root_node.value)
+   # if(root_node.left == None):
+      #ceiling = root_node
+      #floor = None
+    #  return (None, root_node.value)
+    #else:
+      #Go to the left branch
+     # findCeilingFloor(root_node.left, k)
 
+  #if root_node.value > k:
+   # if(root_node.left == None):
+    #  return(None, root_node.value)
+   # elif(root_node.left.left == None):
+    #  if(root_node.left.value > k):
+     #   return(root_node.left.value,root_node.value)
+     # elif(root_node.left.value < k):
+      #  return(root_node.right.value,root_node.value) 
+    #else:
+    #  findCeilingFloor(root_node.left, k)
+ # elif root_node.value < k:
+  #  if(root_node.right == None):
+   #   return(None, root_node.value)
+    #elif(root_node.right.right == None):
+     # if(root_node.right.value < k):
+      #  return(root_node.right.value,root_node.value)
+      #elif(root_node.right.value > k):
+       # return(root_node.left.value,root_node.value)
+    #else:
+     # findCeilingFloor(root_node.right, k)
 
 root = Node(8) 
 root.left = Node(4) 
@@ -56,5 +94,9 @@ root.left.right = Node(6)
 root.right.left = Node(10) 
 root.right.right = Node(14) 
 
-print(findCeilingFloor(root, 5))
+
+globalfloor = None
+globalceiling = None
+
+print(findCeilingFloor(root, 5,globalfloor,globalceiling))
 # (4, 6)
