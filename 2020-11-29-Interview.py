40d3493580051cc9a8e13499bc9b745e9c6f0938
GabrielSuzuki <60160196+GabrielSuzuki@users.noreply.github.com>
2020-11-29 09:59:57 -0800

    initial commit

---
+++ 2020-11-29-Interview.py
@@ -0,0 +1,25 @@
+class MaxStack:
+  def __init__(self):
+    # Fill this in.
+
+  def push(self, val):
+    # Fill this in.
+
+  def pop(self):
+    # Fill this in.
+
+  def max(self):
+    # Fill this in.
+
+s = MaxStack()
+s.push(1)
+s.push(2)
+s.push(3)
+s.push(2)
+print s.max()
+# 3
+s.pop()
+s.pop()
+print s.max()
+# 2
+
