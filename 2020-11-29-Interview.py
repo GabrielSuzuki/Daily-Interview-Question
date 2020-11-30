4297994b4afa7e6c7d028c88aac24c555b6ac85d
GabrielSuzuki <60160196+GabrielSuzuki@users.noreply.github.com>
2020-11-29 16:28:02 -0800

    initial commit

--- 2020-11-29-Interview.py
+++ 2020-11-29-Interview.py
@@ -1,14 +1,34 @@
 class MaxStack:
   def __init__(self):
+    self.head = None
     # Fill this in.
 
   def push(self, val):
+    if not self.head:
+      self.head = val
+      return
+    for current_node in self:
+      pass
+    current_node.next = val
     # Fill this in.
 
   def pop(self):
-    # Fill this in.
+    if not self.head:
+      return
+    for current_node in self:
+      pass
+    current_node = None
 
   def max(self):
+    saved_node = 0
+    if not self.head:
+      return 
+    for current_node in self:
+      if current_node > saved_node:
+        saved_node = current_node
+    return saved_node
+
+    
     # Fill this in.
 
 s = MaxStack()
@@ -16,10 +36,10 @@ s.push(1)
 s.push(2)
 s.push(3)
 s.push(2)
-print s.max()
+print(s.max())
 # 3
 s.pop()
 s.pop()
-print s.max()
+print(s.max())
 # 2
 
